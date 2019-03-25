704c4c004fc3e969f29d9d2675f8e8f8d59d4916
Mikko <Mikko@DESKTOP-6TMVVLD>
2019-03-25 22:49:52 +0200

    commit

---
+++ src/Creatures/cavebug.py
@@ -0,0 +1,14 @@
+from src.Creatures.enemy import Enemy
+from PyQt5.Qt import QPixmap
+from src.animation import Animation
+
+
+class CaveBug(Enemy):
+    
+    def __init__(self,x,y, scene, parent=None):
+        speed = -0.4
+        distance = 10
+        Enemy.__init__(self,scene,speed,distance,parent)
+        self.animation = Animation(self,"Textures/CaveBug",15)
+        self.addPos(x,y)
+    
